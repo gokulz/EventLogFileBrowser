34eef2ffe4207569fe4a3aa5ec27e24045c3142a
gokulakannan877@gmail.com <gokulakannan877@gmail.com>
2025-11-26 10:12:41 +0530

    added-lwc-component

---
+++ force-app/main/default/lwc/eventLogFilters/eventLogFilters.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import fetchEventLogs from '@salesforce/apex/EventLogFileBrowserController.fetchEventLogs';
+
+export default class EventLogFilters extends LightningElement {
+
+    
+
+}
