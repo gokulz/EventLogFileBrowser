79c9660b2d9f320b3a6c6ff54e5fb9e6ffec01ee
gokulakannan877@gmail.com <gokulakannan877@gmail.com>
2025-11-26 10:16:16 +0530

    cleard-apex-action-error

--- force-app/main/default/lwc/eventLogFilters/eventLogFilters.js
+++ force-app/main/default/lwc/eventLogFilters/eventLogFilters.js
@@ -1,8 +1,8 @@
 import { LightningElement } from 'lwc';
-import fetchEventLogs from '@salesforce/apex/EventLogFileBrowserController.fetchEventLogs';
+//import fetchEventLogs from '@salesforce/apex/EventLogFileBrowserController.fetchEventLogs';
 
 export default class EventLogFilters extends LightningElement {
 
-    
+
 
 }
