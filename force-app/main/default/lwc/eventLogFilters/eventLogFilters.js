5dbc3780e697e4a76eebb4e4d8b972ddba159d69
gokulakannan877@gmail.com <gokulakannan877@gmail.com>
2025-11-27 23:09:29 +0530

    added:twofields

--- force-app/main/default/lwc/eventLogFilters/eventLogFilters.js
+++ force-app/main/default/lwc/eventLogFilters/eventLogFilters.js
@@ -1,8 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 //import fetchEventLogs from '@salesforce/apex/EventLogFileBrowserController.fetchEventLogs';
 
 export default class EventLogFilters extends LightningElement {
 
+    @track selectedStartDate='';
+    @track selectedEndDate='';
 
+    handleStartChange(event){
+        this.selectedStartDate = event.target.value;
+        console.log('output start date:  ' + this.selectedStartDate);
+    }
+     handleEndChange(event){
+         this.selectedEndDate = event.target.value;
+         console.log('output end date : ' + this.selectedEndDate);
+     }
 
 }
